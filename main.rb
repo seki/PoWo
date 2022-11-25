60425add7ee4275f6baeba18a46ec99cbccf7381
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2022-11-26 00:40:48 +0900

    test

--- main.rb
+++ main.rb
@@ -2,6 +2,19 @@ require 'webrick'
 require 'tofu'
 require_relative 'src/app'
 
+module Tofu
+  class Context
+    def res_add_cookie(name, value, expires=nil)
+      value = WEBrick::HTTPUtils::escape(value)
+      c = WEBrick::Cookie.new(name, value)
+      c.expires = expires if expires
+      c.path = req_script_name.to_s
+      c.secure = true
+      @res.cookies.push(c)
+    end
+  end
+end
+
 port = Integer(ENV['PORT']) rescue 8000
 server = WEBrick::HTTPServer.new({
   :Port => port,
